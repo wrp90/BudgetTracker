34069d063fe0701f75fc30b44dba7a20a46a1a64
Will Papp <wpapp90@gmail.com>
2021-05-06 17:10:08 -0500

    updated service-worker url

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,6 +7,8 @@ const FILES_TO_CACHE = [
     '/styles.css',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
+    'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
+    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'
 ];
 
 const CACHE_NAME = "static-cache-v2";
